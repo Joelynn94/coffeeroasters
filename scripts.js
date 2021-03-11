0909e1fdafe7289b007d147a1b97f0946dbfa873
Joseph Lynn <josephlynn94@gmail.com>
2021-03-11 08:09:42 -0500

    added toggle btn image src change onclick

--- scripts.js
+++ scripts.js
@@ -1,6 +1,14 @@
 const menuBtn = document.querySelector(".nav__mobile-btn");
 const navMenu = document.querySelector(".nav__mobile-menu");
+const toggleBtnImg = document.querySelector("#toggleBtnImg");
 
 menuBtn.addEventListener('click', () => {
+  // toggle the class "close"
   navMenu.classList.toggle("close");
+  // change image 
+  if (toggleBtnImg.src.match("./assets/shared/mobile/icon-hamburger.svg")) {
+    toggleBtnImg.src = "./assets/shared/mobile/icon-close.svg";
+  } else if (toggleBtnImg.src.match("./assets/shared/mobile/icon-close.svg")) {
+    toggleBtnImg.src = "./assets/shared/mobile/icon-hamburger.svg";
+  }
 });
