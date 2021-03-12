e88e4ef1f048a008230d615a386c0730e0ce83b0
Joseph Lynn <josephlynn94@gmail.com>
2021-03-12 15:56:02 -0500

    added js to grab radio btn values

--- scripts.js
+++ scripts.js
@@ -2,6 +2,21 @@ const menuBtn = document.querySelector(".nav__mobile-btn");
 const navMenu = document.querySelector(".nav__mobile-menu");
 const toggleBtnImg = document.querySelector("#toggleBtnImg");
 
+const preferences = document.getElementsByName("preferences");
+const dataPreference = document.querySelector("#jsPreference");
+
+const beanType = document.getElementsByName("beanType");
+const dataBean = document.querySelector("#jsBean");
+
+const quantity = document.getElementsByName("quantity");
+const dataQuantity = document.querySelector("#jsQuantity");
+
+const grind = document.getElementsByName("grindOption");
+const dataGrind = document.querySelector("#jsGrind");
+
+const deliveries = document.getElementsByName("deliveries");
+const dataDelivery = document.querySelector("#jsDelivery");
+
 menuBtn.addEventListener('click', () => {
   // toggle the class "close"
   navMenu.classList.toggle("close");
@@ -12,3 +27,20 @@ menuBtn.addEventListener('click', () => {
     toggleBtnImg.src = "./assets/shared/mobile/icon-hamburger.svg";
   }
 });
+
+function loopRadioBtns(arr, spanEl) {
+  for (let i = 0; i < arr.length; i++) {
+    element = arr[i];
+
+    if (element.checked) {
+      spanEl.textContent = element.value;
+      console.log(element.value);
+    }
+  }
+}
+
+loopRadioBtns(preferences, dataPreference);
+loopRadioBtns(beanType, dataBean);
+loopRadioBtns(quantity, dataQuantity);
+loopRadioBtns(grind, dataGrind);
+loopRadioBtns(deliveries, dataDelivery);
