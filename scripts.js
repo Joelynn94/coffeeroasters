43369954e46243c43e16c70f08215b876bda31cf
Joseph Lynn <josephlynn94@gmail.com>
2021-03-14 08:37:37 -0400

    did js cleanup and now localstorage is not changing onclick

--- scripts.js
+++ scripts.js
@@ -2,26 +2,31 @@ const menuBtn = document.querySelector(".nav__mobile-btn");
 const navMenu = document.querySelector(".nav__mobile-menu");
 const toggleBtnImg = document.querySelector("#toggleBtnImg");
 const options = document.getElementsByClassName("options");
-
-const preferences = document.getElementsByName("preferences");
-let dataPreference = document.querySelector(".jsPreference");
-
-const beanType = document.getElementsByName("beanType");
-let dataBean = document.querySelector(".jsBean");
-
-const quantity = document.getElementsByName("quantity");
-let dataQuantity = document.querySelector(".jsQuantity");
-
-const grind = document.getElementsByName("grindOption");
-let dataGrind = document.querySelector(".jsGrind");
-
-const deliveries = document.getElementsByName("deliveries");
-let dataDelivery = document.querySelector(".jsDelivery");
-
 const trigger = document.querySelector("[data-open]");
 const modal = document.querySelector(".modal");
 
-menuBtn.addEventListener('click', () => {
+// input radio names 
+const preferences = document.getElementsByName("preferences");
+const beanType = document.getElementsByName("beanType");
+const quantity = document.getElementsByName("quantity");
+const grind = document.getElementsByName("grindOption");
+const deliveries = document.getElementsByName("deliveries");
+
+// order summary span elements 
+const dataPreference = document.querySelector(".jsPreference");
+const dataBean = document.querySelector(".jsBean");
+const dataQuantity = document.querySelector(".jsQuantity");
+const dataGrind = document.querySelector(".jsGrind");
+const dataDelivery = document.querySelector(".jsDelivery");
+
+// modal span elements 
+const modalDataPreference = document.querySelector(".jsModalPreference");
+const modalDataBean = document.querySelector(".jsModalBean");
+const modalDataQuantity = document.querySelector(".jsModalQuantity");
+const modalDataGrind = document.querySelector(".jsModalGrind");
+const modalDataDelivery = document.querySelector(".jsModalDelivery");
+
+function toggleMenuBtn() {
   // toggle the class "close"
   navMenu.classList.toggle("close");
   // change image 
@@ -30,24 +35,59 @@ menuBtn.addEventListener('click', () => {
   } else if (toggleBtnImg.src.match("./assets/shared/mobile/icon-close.svg")) {
     toggleBtnImg.src = "./assets/shared/mobile/icon-hamburger.svg";
   }
-});
+}
 
 let userOptions = {};
+
+// save object to local storage 
+function saveToLocalStorage(obj) {
+  // stringify the object to be able to save to local storage 
+  const userOptionsStringify = JSON.stringify(obj);
+
+  // save userOptions object to local storage 
+  localStorage.setItem('userOptions', userOptionsStringify);
+}
+
+function renderOrderSummary(obj) {
+  dataPreference.textContent = obj.userPreference;
+  dataBean.textContent = obj.userBeanType;
+  dataQuantity.textContent = obj.userQuantity;
+  dataGrind.textContent = obj.userGrindOption;
+  dataDelivery.textContent = obj.userDelivery;
+}
+
+function renderModalOrderSummary(obj) {
+  modalDataPreference.textContent = obj.userPreference;
+  modalDataBean.textContent = obj.userBeanType;
+  modalDataQuantity.textContent = obj.userQuantity;
+  modalDataGrind.textContent = obj.userGrindOption;
+  modalDataDelivery.textContent = obj.userDelivery;
+}
+
+function getFromLocalStorage() {
+  // turn the userOptions back into an object 
+  const userOptionsObj = JSON.parse(localStorage.getItem("userOptions"));
+
+  // If userOptions were retrieved from localsotrage, update the userOptions object to it
+  if (userOptionsObj !== null) {
+    userOptions = userOptionsObj;
+  }
+
+  // render the order summary
+  renderOrderSummary(userOptionsObj);
+  renderModalOrderSummary(userOptionsObj);
+}
+
+getFromLocalStorage();
 
 function loopRadioBtns(arr, spanEl) {
   for (let i = 0; i < arr.length; i++) {
     const element = arr[i];
 
+    // see if an element is checked 
     if (element.checked) {
       spanEl.textContent = element.value;
       console.log(element.value);
-      return userOptions = {
-        userPreference: dataPreference.textContent,
-        userBeanType: dataBean.textContent,
-        userQuantity: dataQuantity.textContent,
-        userGrindOption: dataGrind.textContent,
-        userDelivery: dataDelivery.textContent
-      };
     }
   }
 }
@@ -62,32 +102,31 @@ function callLoopRadioBtns() {
 
 callLoopRadioBtns();
 
-for (let i = 0; i < options.length; i++) {
-  options[i].addEventListener("click", (e) => {
-    const target = e.target;
-    console.log(target);
+function loopOptionDivs() {
+  for (let i = 0; i < options.length; i++) {
+    options[i].addEventListener("click", (e) => {
+      const target = e.target;
+      console.log(target);
 
-    if (target.type === "radio") {
-      callLoopRadioBtns();
-    };
-    let userOptionsSerialized = JSON.stringify(userOptions);
-    localStorage.setItem('userOptions', userOptionsSerialized);
-    let userOptionsReversed = JSON.parse(localStorage.getItem("userOptions"));
-    console.log(userOptionsReversed);
-    userOptionsDOM(userOptionsReversed);
-  });
+      if (target.type === "radio") {
+        callLoopRadioBtns();
+        saveToLocalStorage(userOptions);
+        getFromLocalStorage();
+      };
+    });
+  }
 }
 
-function userOptionsDOM(obj) {
-  dataPreference.textContent = obj.userPreference;
-  dataBean.textContent = obj.userBeanType;
-  dataQuantity.textContent = obj.userQuantity;
-  dataGrind.textContent = obj.userGrindOption;
-  dataDelivery.textContent = obj.userDelivery;
-}
+loopOptionDivs();
 
 function toggleModal() {
   modal.classList.toggle("is-visible");
+
+  if (modal.classList.contains("is-visible")) {
+    getFromLocalStorage();
+  } else {
+    return;
+  }
 }
 
 function modalClick(e) {
@@ -96,6 +135,7 @@ function modalClick(e) {
   }
 }
 
+menuBtn.addEventListener("click", toggleMenuBtn);
 trigger.addEventListener("click", toggleModal);
 window.addEventListener("click", modalClick);
 
